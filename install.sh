48884d9bfae3751c2be1abb4219ede054c5cdc9a
Mariusz Skoneczko <mariusz@skoneczko.com>
2018-11-16 15:35:37 +1100

    Add Xmodmap configuration to treat Caps Lock as left control

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script to install dotfiles using symlinks
 
-files="bashrc bash_profile bash_aliases gitconfig gitignore_global npmrc" # without dots
+files="bashrc bash_profile bash_aliases gitconfig gitignore_global npmrc Xmodmap" # without dots
 files+=" config/mpv/mpv.conf config/mpv/input.conf"
 
 cd ~
