e295ee5941546eea3862c6a1f332ea6d19c39774
Mariusz Skoneczko <mariusz@skoneczko.com>
2020-02-05 09:10:56 +1100

    Add basic mpd configuration

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # script to install dotfiles using symlinks
 
 files="bashrc bash_profile bash_aliases gitconfig gitignore_global npmrc Xmodmap" # without dots
-files+=" config/mpv config/termite vim"
+files+=" config/mpv config/termite config/mpd/mpd.conf vim"
 
 cd ~
 
