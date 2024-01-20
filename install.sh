1463c46985fc6a4eddf5a572e229f998b69f270b
Mariusz Skoneczko <mariusz@skoneczko.com>
2024-01-20 11:56:27 +1100

    Migrate alacritty to toml

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # script to install dotfiles using symlinks
 
 files="bashrc bash_profile bash_aliases gitconfig gitignore_global npmrc Xmodmap" # without dots
-files+=" config/mpv config/alacritty/alacritty.yml config/mpd/mpd.conf vim"
+files+=" config/mpv config/alacritty/alacritty.toml config/mpd/mpd.conf vim"
 
 cd ~
 
